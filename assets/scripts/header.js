ba5c4baa817f8de9bc9bdfc27cdd3345d9a4a91b
julesodea <jules.odea@gmail.com>
2019-04-09 10:56:37 +1200

    revert changes

---
+++ assets/scripts/header.js
@@ -0,0 +1,40 @@
+// TypeIT //
+
+new TypeIt('#breakLines', {
+    strings: ["Explore Nearby", "Select an Icon to get started"],
+    speed: 60,
+    waitUntilVisible: true,
+    cursor: false
+}).go();
+
+// End of TypeIT //
+
+let menu = document.getElementById('menuBtn');
+let openMenu = document.getElementById('menuOpen');
+let type = document.getElementById('typeText');
+let marker = document.getElementById('marker');
+let panels = document.getElementById('panel');
+menu.addEventListener("click", function () {
+    type.classList.toggle("hide");
+    marker.classList.toggle("hide");
+    openMenu.classList.toggle("hide");
+    panels.classList.toggle("hide");
+})
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function () {
+    // Toggle class "is-active"
+    hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+});
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3
+
+
+
+
+})
