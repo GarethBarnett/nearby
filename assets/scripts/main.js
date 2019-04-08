70f3eeae73a3e4837ae572a0bffdb687d1fe3dda
GarethBarnett <osd@hotmail.co.nz>
2019-04-08 19:22:41 +1200

    splash js & animation, phone overlay

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,20 @@
 /* Main JS Files */
+
+
+let loader = document.getElementById("splash");
+
+
+/** Splash Animation */
+setTimeout(function () {
+   'use strict';
+   loader.classList.add('slideOutUp');
+}, 3000);
+
+
+/** Splash Removal */
+setTimeout(function () {
+   'use strict';
+   loader.parentNode.removeChild(loader);
+}, 4000);
+
+
