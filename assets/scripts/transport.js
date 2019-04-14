ad7480c210288d282a7ff5195aa8a5bf2a7b7ce9
linmjnz@gmail.com <linmjnz@gmail.com>
2019-04-15 09:39:53 +1200

    initial at api setup

---
+++ assets/scripts/transport.js
@@ -0,0 +1,69 @@
+/* Jason */
+/* findBus function is using Auckland Transport API to find the nearby buses according to */
+/* user current location */
+function findBus() {
+    var params = {};
+    $.ajax({
+        url: "https://api.at.govt.nz/v2/public/realtime/vehiclelocations?" + $.param(params),
+        beforeSend: function(xhrObj){
+            /* Request headers */
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","0011a2284d7a4d8caf839d6bb94428e1");
+        },
+        type: "GET",
+        /* Request body */
+        data: "{body}",
+    })
+    .done(function(data) {
+        console.log(data)
+        let returnedData = data.response.entity;
+            let pData = data.response.entity[0].vehicle.position;
+            positionX = pData.latitude;
+            positionY = pData.longitude;
+            for(let i = 0; i < returnedData.length; i++){
+                if((returnedData[i].vehicle.position.latitude <= (latUser+ 0.005)) && (returnedData[i].vehicle.position.latitude >= (latUser - 0.005))){
+                    if((returnedData[i].vehicle.position.longitude <= (lngUser + 0.005)) && (returnedData[i].vehicle.position.longitude >= (lngUser - 0.005))){
+                        busMarker = new google.maps.Marker({
+                            map: map,
+                            draggable: false,
+                            icon: {url: icons.bus, scaledSize: new google.maps.Size(30, 30)},
+                            position: {lat: returnedData[i].vehicle.position.latitude, lng: returnedData[i].vehicle.position.longitude}
+                        });
+                        busLocation.lat = returnedData[i].vehicle.position.latitude;
+                        busLocation.lng = returnedData[i].vehicle.position.longitude;
+                        return;
+                    }
+                }
+            } 
+    })
+    .fail(function() {
+        alert("error");
+    });
+}
+/* Jason */
+
+/* Jason */
+/* findBusStop is trying to find the nearest bus stop according to user current location by AT API */
+function findBusStop(lat,lng, distance){
+    $.ajax({
+        url: "https://api.at.govt.nz/v2/gtfs/stops/geosearch?lat=" + lat + "&lng=" + lng + "&distance=" + distance,
+        beforeSend: function(xhrObj){
+            /* Request headers */
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","0011a2284d7a4d8caf839d6bb94428e1");
+        },
+        type: "GET",
+        /* Request body */
+        data: "{body}",
+    })
+    .done(function(data) {
+        busStopMarker = new google.maps.Marker({
+            map: map,
+            draggable: false,
+            icon: {url: icons.busstop, scaledSize: new google.maps.Size(40, 40)},
+            position: {lat: data.response[0].stop_lat, lng: data.response[0].stop_lon}
+        });
+    })
+    .fail(function() {
+        alert("error");
+    });
+}
+/* Jason */
